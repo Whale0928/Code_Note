44ad4a9b84c9a70729ce063f413ae68490179da0
KIM HYUN GI <rlagusrl928@gmail.com>
2022-09-30 19:28:58 +0900

    DFS/BFS

---
+++ src/com/programmers/java/basic/FsMain.java
@@ -0,0 +1,21 @@
+package src.com.programmers.java.basic;
+
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class FsMain {
+    /**
+     * 깊이 우선 탐색 dfs
+     * 너비 우선 탐색 bfs
+     */
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>(); // 클래스라 바로 사용 가능
+
+        Queue<Integer> Queue = new LinkedList<>(); // 인터페이스
+        Deque<Integer> dq = new LinkedList<>();
+    }
+}
