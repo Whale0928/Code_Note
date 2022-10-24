e6605d33c4261c101d48865188db8f37c3e29fcd
whale0928 <rlagusrl928@gmail.com>
2022-10-24 18:58:24 +0900

    프로그래머스 -내 마음대로 문자열 정렬하기

---
+++ src/com/programmers/java/quest/sortIWish.java
@@ -0,0 +1,55 @@
+package src.com.programmers.java.quest;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class sortIWish {
+    /**
+     * 문자열로 구성된 리스트 strings와, 정수 n이 주어졌을 때,
+     * 각 문자열의 인덱스 n번째 글자를 기준으로 오름차순 정렬하려 합니다.
+     * 예를 들어 strings가 ["sun", "bed", "car"]이고 n이 1이면
+     * 각 단어의 인덱스 1의 문자 "u", "e", "a"로 strings를 정렬합니다.
+     * <p>
+     * 제한 조건
+     * strings는 길이 1 이상, 50이하인 배열입니다.
+     * strings의 원소는 소문자 알파벳으로 이루어져 있습니다.
+     * strings의 원소는 길이 1 이상, 100이하인 문자열입니다.
+     * 모든 strings의 원소의 길이는 n보다 큽니다.
+     * 인덱스 1의 문자가 같은 문자열이 여럿 일 경우, 사전순으로 앞선 문자열이 앞쪽에 위치합니다.
+     * strings	n	return
+     * ["sun", "bed", "car"]	1	["car", "bed", "sun"]
+     * ["abce", "abcd", "cdx"]	2	["abcd", "abce", "cdx"]
+     * 입출력 예 설명
+     * 입출력 예 1
+     * "sun", "bed", "car"의 1번째 인덱스 값은 각각 "u", "e", "a" 입니다.
+     * 이를 기준으로 strings를 정렬하면 ["car", "bed", "sun"] 입니다.
+     *
+     * 입출력 예 2
+     * "abce"와 "abcd", "cdx"의 2번째 인덱스 값은 "c", "c", "x"입니다. 따라서 정렬 후에는 "cdx"가 가장 뒤에 위치합니다. "abce"와 "abcd"는 사전순으로 정렬하면 "abcd"가 우선하므로, 답은 ["abcd", "abce", "cdx"] 입니다.
+     */
+    public static void main(String[] args) {
+        //String[] arr = {"sun", "bed", "car"};
+        String[] arr = {"abcd", "abce", "cdx"};
+        sortIWish sortIWish = new sortIWish();
+        String[] result = sortIWish.solution(arr,1);
+        System.out.println(Arrays.toString(result));
+    }
+
+    public String[] solution(String[] strings, int n) {
+        String[] answer = new String[strings.length];
+        //key : value로 다루기 위해 map을 활용
+        Map<Character,String> map = new HashMap<>();
+
+        for(String s : strings){
+            map.put(s.charAt(n),s);
+        }
+
+        List<Character> list = map.keySet().stream().sorted(Character::compareTo).collect(Collectors.toList());
+        for(int i=0; i<strings.length; i++) answer[i] = map.get(list.get(i));
+
+        return answer;
+    }
+}
